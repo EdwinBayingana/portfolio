9b9acb013cf56751692e18510ba946112878b601
EdwinBayingana <bayinganaedwin@gmail.com>
2023-10-01 14:11:07 +0200

    position the navbar on both large and small devices

--- components/Nav.js
+++ components/Nav.js
@@ -1,23 +1,43 @@
-//  links
-// const links = [
-//   { name: 'home', path: '/', icon: <HiHome /> },
-//   { name: 'about', path: '/about', icon: <HiUser /> },
-//   { name: 'services', path: '/services', icon: <HiRectangleGroup /> },
-//   { name: 'work', path: '/work', icon: <HiViewColumns /> },
-//   {
-//     name: 'testimonials',
-//     path: '/testimonials',
-//     icon: <HiChatBubbleBottomCenterText />,
-//   },
-//   {
-//     name: 'contact',
-//     path: '/contact',
-//     icon: <HiEnvelope />,
-//   },
-// ];
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import {
+  HiHome,
+  HiUser,
+  HiViewColumns,
+  HiRectangleGroup,
+  HiChatBubbleBottomCenterText,
+  HiEnvelope,
+} from 'react-icons/hi2';
+
+export const navData = [
+  { name: 'home', path: '/', icon: <HiHome /> },
+  { name: 'about', path: '/about', icon: <HiUser /> },
+  { name: 'services', path: '/services', icon: <HiRectangleGroup /> },
+  { name: 'work', path: '/work', icon: <HiViewColumns /> },
+  {
+    name: 'testimonials',
+    path: '/testimonials',
+    icon: <HiChatBubbleBottomCenterText />,
+  },
+  {
+    name: 'contact',
+    path: '/contact',
+    icon: <HiEnvelope />,
+  },
+];
 
 const Nav = () => {
-  return <nav>nav</nav>;
+  const router = useRouter();
+
+  return (
+    <nav className="flex flex-col items-center xl:justify-center gap-y-4 fixed h-max bottom-0 mt-auto xl:right-[2%] z-50 top-0 w-full xl:w-16 xl:max-w-md xl:h-screen">
+      <div className="flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 px-4 md:px-40 xl:px-0 h-[80px] xl:h-max py-8 bg-white/10 backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full">
+        {navData.map((link, index) => {
+          return <Link href={link.path}>{link.icon}</Link>;
+        })}
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
