18c3c0fa7bc5fb3b8db41ea2b5d2064a0fc7f713
EdwinBayingana <bayinganaedwin@gmail.com>
2023-10-08 12:28:04 +0200

    add the skills subheaders

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,5 +1,22 @@
-//  data
-const aboutData = [
+import React, { useState } from 'react';
+
+import {
+  FaHtml5,
+  FaCss3,
+  FaJs,
+  FaReact,
+  FaWordpress,
+  FaFigma,
+} from 'react-icons/fa';
+
+import {
+  SiNextdotjs,
+  SiFramer,
+  SiAdobexd,
+  SiAdobephotoshop,
+} from 'react-icons/si';
+
+export const aboutData = [
   {
     title: 'skills',
     info: [
@@ -70,8 +87,50 @@ const aboutData = [
   },
 ];
 
+import Avatar from '../../components/Avatar';
+import Circles from '../../components/Circles';
+
+import { motion } from 'framer-motion';
+import { fadeIn } from '../../variants';
+
 const About = () => {
-  return <div>About</div>;
+  const [index, setIndex] = useState(0);
+  return (
+    <div className="h-full bg-primary/30 py-32 text-center xl:text-left">
+      <Circles />
+      {/* Avatar img */}
+      <motion.div
+        variants={fadeIn('right', 0.2)}
+        initial="hidden"
+        animate="show"
+        exit="hidden"
+        className="hidden xl:flex absolute bottom-0 -left-[370px]"
+      >
+        <Avatar />
+      </motion.div>
+      <div className="container mx-auto h-full flex flex-col items-center xl:flex-row">
+        <div>text</div>
+        <div>
+          <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
+            {aboutData.map((item, itemIndex) => {
+              return (
+                <div
+                  key={itemIndex}
+                  className={`${
+                    index === itemIndex &&
+                    'text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300'
+                  } cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`}
+                  onClick={() => setIndex(itemIndex)}
+                >
+                  {item.title}
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
