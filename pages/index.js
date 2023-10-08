664ff0d5a4c31bb843b9314880d04d37ab0b6d0c
EdwinBayingana <bayinganaedwin@gmail.com>
2023-10-08 10:30:39 +0200

    add motion to the projects button

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,57 @@
 import Image from 'next/image';
 
+import ParticlesContainer from '../components/ParticlesContainer';
+import ProjectsBtn from '../components/ProjectsBtn';
+import Avatar from '../components/Avatar';
+
+import { motion } from 'framer-motion';
+
+import { fadeIn } from '../variants';
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="bg-primary/60 h-full ">
+      {/* text */}
+      <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
+        <div className="text-center flex flex-col justify-center xl:text-left h-full container mx-auto">
+          {/* title */}
+          <motion.h1 className="h1">
+            Transforming Ideas <br />
+            Into <span className="text-accent"> Digital Reality</span>
+          </motion.h1>
+          {/* subtitle */}
+          <motion.p
+            variants={fadeIn('down', 0.3)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16"
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin et mi
+            molestie, feugiat metus eu, volutpat mi. Suspendisse imperdiet
+            viverra augue, vitae elementum dolor feugiat in. Morbi vestibulum
+            tempus neque, semper faucibus tellus vulputate eget.
+          </motion.p>
+          {/* btn */}
+          <div className="flex justify-center xl:hidden relative">
+            <ProjectsBtn />
+          </div>
+          <motion.div
+            variants={fadeIn('down', 0.4)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="hidden xl:flex"
+          >
+            <ProjectsBtn />
+          </motion.div>
+        </div>
+      </div>
+
+      {/* image */}
+      <div>image</div>
+    </div>
+  );
 };
 
 export default Home;
