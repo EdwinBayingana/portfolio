2ef6fb0f1de9eb06268b3e0df36034a0a9fe8405
EdwinBayingana <bayinganaedwin@gmail.com>
2023-10-08 11:56:59 +0200

    finish the header with static social links and dummy logo to change later on

--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,38 @@
+import Link from 'next/link';
+
+import {
+  RiInstagramLine,
+  RiFacebookLine,
+  RiTwitterLine,
+  RiGithubLine,
+  RiGmailLine,
+} from 'react-icons/ri';
+import { BiLogoGmail, BiLogoWhatsapp } from 'react-icons/bi';
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return (
+    <div className="flex items-center gap-x-5 text-lg">
+      <Link href={''} className="hover:text-accent transition-all duration-300">
+        <RiGithubLine />
+      </Link>
+      <Link href={''} className="hover:text-accent transition-all duration-300">
+        <BiLogoGmail />
+      </Link>
+      <Link href={''} className="hover:text-accent transition-all duration-300">
+        <RiInstagramLine />
+      </Link>
+      {/* <Link href={''} className="hover:text-accent transition-all duration-300">
+        <RiTwitterLine />
+      </Link> */}
+      <Link href={''} className="hover:text-accent transition-all duration-300">
+        <BiLogoWhatsapp />
+      </Link>
+
+      <Link href={''} className="hover:text-accent transition-all duration-300">
+        <RiFacebookLine />
+      </Link>
+    </div>
+  );
 };
 
 export default Socials;
--- pages/index.js
+++ pages/index.js
@@ -59,8 +59,8 @@ const Home = () => {
         {/* bg image 👇🏽*/}
         <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"></div>
 
-        {/* Particles */}
-        <ParticlesContainer />
+        {/* Particles (🚨: this feature is causing the browser to overheat) */}
+        {/* <ParticlesContainer /> */}
 
         {/* Avatar Image 👇🏽*/}
         {/* <motion.div
