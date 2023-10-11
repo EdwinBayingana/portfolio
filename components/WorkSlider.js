840293d38afe429c793940960744161e2d3a2983
EdwinBayingana <bayinganaedwin@gmail.com>
2023-10-11 19:26:49 +0200

    finish the work section

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -42,6 +42,7 @@ const workSlides = {
     },
   ],
 };
+
 import Image from 'next/image';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination } from 'swiper';
@@ -68,11 +69,29 @@ const WorkSlider = () => {
               {slide.images.map((image, index) => {
                 return (
                   <div className="relative rounded-lg overflow-hidden flex items-center justify-center group">
-                    <div className="flex items-center justify-center relative overflow-hidden">
+                    <div
+                      className="flex items-center justify-center relative overflow-hidden group"
+                      key={index}
+                    >
                       {/* image */}
                       <Image src={image.path} width={500} height={300} alt="" />
                       {/* overlay gradient */}
                       <div className="absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700"></div>
+                      {/* title */}
+                      <div className="absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300">
+                        <div className="flex items-center gap-x-2 text-[13px] tracking-[0.2em]">
+                          {/* title part 1 */}
+                          <div className="delay-100">LIVE</div>
+                          {/* title part 2 */}
+                          <div className="translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150">
+                            PROJECT
+                          </div>
+                          {/* icon */}
+                          <div className="text-xl translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-200">
+                            <BsArrowRight />
+                          </div>
+                        </div>
+                      </div>
                     </div>
                   </div>
                 );
