7b82a08d158dcd5a0a0062d031deef782044800e
EdwinBayingana <bayinganaedwin@gmail.com>
2023-10-11 18:38:22 +0200

    complete the services section with motion included

--- components/Bulb.js
+++ components/Bulb.js
@@ -1,5 +1,16 @@
+import Image from 'next/image';
+
 const Bulb = () => {
-  return <div>Bulb</div>;
+  return (
+    <div className="absolute z-10 w-[200px] xl:w-[260px] -left-36 -bottom-12 rotate-12 mix-blend-color-dodge animate-pulse duration-75">
+      <Image
+        src={'/bulb.png'}
+        width={260}
+        height={200}
+        className="w-full h-full"
+      />
+    </div>
+  );
 };
 
 export default Bulb;
