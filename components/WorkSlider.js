80eef8b7435acacf939519040a99ff7c42d352f5
EdwinBayingana <bayinganaedwin@gmail.com>
2023-10-11 19:05:42 +0200

    map work images in slider

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -42,38 +42,44 @@ const workSlides = {
     },
   ],
 };
+import Image from 'next/image';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination } from 'swiper';
+import { BsArrowRight } from 'react-icons/bs';
 
 import 'swiper/css';
 import 'swiper/css/free-mode';
 import 'swiper/css/pagination';
 
-import { BsArrowRight } from 'react-icons/bs';
-
 const WorkSlider = () => {
   return (
     <Swiper
-      breakpoints={{
-        320: {
-          slidesPerView: 1,
-          spaceBetween: 15,
-        },
-
-        640: {
-          slidesPerView: 3,
-          spaceBetween: 15,
-        },
-      }}
-      freeMode={true}
+      spaceBetween={10}
       pagination={{
         clickable: true,
       }}
       modules={[Pagination]}
-      className="h-[240px] md:h-[340px]"
+      className="h-[280px] md:h-[480px]"
     >
       {workSlides.slides.map((slide, index) => {
-        return <SwiperSlide key={index}>slide</SwiperSlide>;
+        return (
+          <SwiperSlide key={index}>
+            <div className="grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer">
+              {slide.images.map((image, index) => {
+                return (
+                  <div className="relative rounded-lg overflow-hidden flex items-center justify-center group">
+                    <div className="flex items-center justify-center relative overflow-hidden">
+                      {/* image */}
+                      <Image src={image.path} width={500} height={300} alt="" />
+                      {/* overlay gradient */}
+                      <div className="absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700"></div>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </SwiperSlide>
+        );
       })}
     </Swiper>
   );
